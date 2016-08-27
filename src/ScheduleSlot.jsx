0abce6f9ea8b94d4ebb75a62f1977337c728a545
Caleb Champlin <caleb.champlin@gmail.com>
2016-08-27 09:58:34 -0600

    Fix wrong file names

--- src/ScheduleSlot.jsx
+++ src/ScheduleSlot.jsx
@@ -1,17 +1,14 @@
 import React, { PropTypes, Component } from 'react'
 import cn from 'classnames'
 
-export default class TimeSlot extends Component {
+export default class ScheduleSlot extends Component {
   static propTypes = {
-    value: PropTypes.instanceOf(Date).isRequired,
-    isNow: PropTypes.bool,
     showLabel: PropTypes.bool,
     content: PropTypes.string,
     culture: PropTypes.string
   }
 
   static defaultProps = {
-    isNow: false,
     showLabel: false,
     content: ''
   }
@@ -21,8 +18,7 @@ export default class TimeSlot extends Component {
       <div
         className={cn(
           'rbc-time-slot',
-          this.props.showLabel && 'rbc-label',
-          this.props.isNow && 'rbc-now',
+          this.props.showLabel && 'rbc-label'
         )}
       >
       {this.props.showLabel &&
