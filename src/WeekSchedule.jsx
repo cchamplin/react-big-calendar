35d4f8991682972ae643ee38bd9c3d1e0f75b28d
Caleb Champlin <caleb.champlin@gmail.com>
2016-08-27 09:43:42 -0600

    Fix wrong reference

--- src/WeekSchedule.jsx
+++ src/WeekSchedule.jsx
@@ -15,7 +15,7 @@ let ScheduleWeek = React.createClass({
 
   render() {
     let { date } = this.props
-    let { start, end } = Week.range(date, this.props)
+    let { start, end } = WeekSchedule.range(date, this.props)
 
     return (
       <ScheduleGrid {...this.props} start={start} end={end} eventOffset={15}/>
